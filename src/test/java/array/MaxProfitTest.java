795d64968aea640f76d636a4da33eae61f5a9335
zhangyu1 <zhangyu1@yunzhangfang.com>
2021-09-28 14:42:31 +0800

    day04_买卖股票的最佳时机

---
+++ src/test/java/array/MaxProfitTest.java
@@ -0,0 +1,25 @@
+package array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author ：zhangyu
+ * @date ：Created in 2021/9/28 14:39
+ * @description：
+ */
+public class MaxProfitTest {
+
+    @Test
+    public void maxProfit() {
+        int prices1[] = {2, 5, 1, 3};
+        assertEquals(3, MaxProfit.maxProfit(prices1));
+
+        int prices2[] = {7, 1, 5, 3, 6, 4};
+        assertEquals(5, MaxProfit.maxProfit(prices2));
+
+        int prices3[] = {7, 6, 4, 3, 1};
+        assertEquals(0, MaxProfit.maxProfit(prices3));
+    }
+}
